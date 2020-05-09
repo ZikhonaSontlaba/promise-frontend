179417baafd04c071995f5ef6841422008bbf347
Thagana <samuelmthwa79@gmail.com>
2020-05-09 22:21:34 +0200

    Updates

---
+++ precache-manifest.945be7aff97efdb383ff1dc9df61189d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bee4a429585c8be74433caf4895d097",
+    "url": "/promise-frontend/index.html"
+  },
+  {
+    "revision": "3ab76ce1838e1d97f6a0",
+    "url": "/promise-frontend/static/js/2.4445523a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/promise-frontend/static/js/2.4445523a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c7417686621e9b4226",
+    "url": "/promise-frontend/static/js/main.fde3e2c7.chunk.js"
+  },
+  {
+    "revision": "9fe33343368c9cac2b8d",
+    "url": "/promise-frontend/static/js/runtime-main.ce9144e6.js"
+  },
+  {
+    "revision": "29bb20103322fb4b5e60d1857c2b8ab1",
+    "url": "/promise-frontend/static/media/Logo.29bb2010.jpg"
+  }
+]);
